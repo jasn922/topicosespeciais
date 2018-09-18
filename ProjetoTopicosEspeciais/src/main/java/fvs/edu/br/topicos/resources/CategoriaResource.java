724edb8439ff371a0c5437d577c6b03319326da8
Joaquim Augusto <J.augustonetto@hotmail.com>
2018-09-18 20:07:17 -0300

    Implementação da camada de serviços de categoria

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/CategoriaResource.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/CategoriaResource.java
@@ -1,35 +1,34 @@
 package fvs.edu.br.topicos.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import fvs.edu.br.topicos.domain.Categoria;
+import fvs.edu.br.topicos.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Bombril");
-		Categoria cat2 = new Categoria(2, "Detergente");
-		// Categoria cat3 = new Categoria(3, "Bombril");
-		// Categoria cat4 = new Categoria(4, "Bombril");
-		// Categoria cat5 = new Categoria(5, "Bombril");
-		// Categoria cat6 = new Categoria(6, "Bombril");
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
 
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
+		Categoria obj = service.buscar(id);
 
-		return lista;
+		return ResponseEntity.ok().body(obj);
+		
+		
 
 	}
+	
+	
 
 }
