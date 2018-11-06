841b90a77a8f89670d6da5a262a5325d2faf8fa9
Joaquim Augusto <j.augustonetto@hotmail.com>
2018-11-06 20:56:00 -0200

    Implementação Recurso e Serviço de Pedido

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/PedidoResource.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/PedidoResource.java
@@ -1,10 +1,26 @@
 package fvs.edu.br.topicos.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import fvs.edu.br.topicos.domain.Pedido;
+import fvs.edu.br.topicos.services.PedidoService;
 
 @RestController
 @RequestMapping(value = "/pedidos")
 public class PedidoResource {
+	@Autowired
+	private PedidoService service;
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Pedido obj = service.buscar(id);
+
+		return ResponseEntity.ok().body(obj);
+
+	}
 }
